725ba7fea6cadc65dd3ebbf9951bcf8861559f5f
GLetiwa <graceletiwa@gmail.com>
2024-04-11 20:50:51 +0300

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,7 @@ def pascal_triangle(n):
     """handles the pascals triangle expansion"""
     if n <= 0:
         return []
-    triangle = [[1]]
+    triangle = [[1]] # first element of each row is always 1
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
@@ -14,3 +14,8 @@ def pascal_triangle(n):
         triangle.append(row)
 
     return triangle
+
+# test function
+triangle = pascal_triangle(5)
+for row in triangle:
+    print("[{}]".format(",".join([str(x) for x in row])))
