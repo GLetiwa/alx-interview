ed7dac225856fb64620f7329df2b94ba5fab04f2
GLetiwa <graceletiwa@gmail.com>
2024-04-24 15:34:51 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,14 +5,8 @@
 def minOperations(n):
     """dynamic programming"""
     if n <= 1:
-        return n
-
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
+        return 0
 
     for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n]
+        if n % i == 0:
+            return minOperations(int(n / i)) + i
